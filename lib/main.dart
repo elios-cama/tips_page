b9d9a7d4d8c34d94846e26f5e0888d1a72693452
elios-cama <elios.cama@gmail.com>
2020-11-16 13:00:28 +0100

    added phone icon

--- lib/main.dart
+++ lib/main.dart
@@ -33,21 +33,51 @@ class MyApp extends StatelessWidget {
               ),
               Card(
                 
-                child: Column(
-                  children: <Widget>[
-                    Text('Nouveautés'),
-                    Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                          child: Image.asset('images/ios14.png')),
+                child: Container(
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('images/ios14.png')
+                    )
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left : 15.0,top: 10.0),
+                    child: Text(
+                        'Nouveautés',
+                        style : TextStyle(
+                          fontSize: 30.0,
+                          color : Colors.white,
+                          fontWeight: FontWeight.bold,
                     ),
 
-                  ],
+                    ),
+                  ),
                 ),
                 
                   
                 margin: EdgeInsets.only(left: 20.0, right: 20.0,top : 5.0),
                 
+              ),
+              Row(
+                children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left :20.0, top:15.0, bottom: 15.0),
+                      child: Container(
+
+                        height: 100,
+                        width: 100.0,
+                        color: Colors.grey.shade900,
+                        child: Center(
+                          child: Image(
+                            image: AssetImage('images/phone.png'),
+                          ),
+                        ),
+                      ),
+                    )
+                ],
               )
             ],
           ),
